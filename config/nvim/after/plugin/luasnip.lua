5d299e00dbbef98ce1290ccba2e5079273cae98f
Nikin Baidar <nikinbaidarr@gmail.com>
2022-12-21 12:09:21 +0545

    init writing luasnippets

--- config/nvim/after/plugin/luasnip.lua
+++ config/nvim/after/plugin/luasnip.lua
@@ -6,7 +6,6 @@
 local luasnip = require("luasnip")
 --
 -- Global short hands.
-luasnippets = luasnip.add_snippets
 snippet = luasnip.snippet
 fn_node = luasnip.function_node
 tx_node = luasnip.text_node
@@ -14,7 +13,7 @@ in_node = luasnip.insert_node
 
 local parameters = { noremap = ture, silent = true}
 
-vim.keymap.set({"i", "s"}, "<Tab>",
+vim.keymap.set({"i", "s"}, "<C-f>",
 function()
     if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -22,7 +21,7 @@ function()
 end,
 parameters)
 
-vim.keymap.set({"i", "s"}, "<S-Tab>",
+vim.keymap.set({"i", "s"}, "<C-z>",
 function()
     if luasnip.jumpable(-1) then
         luasnip.jump(-1)
@@ -30,25 +29,126 @@ function()
 end,  
 parameters)
 
+--
+
 require("luasnip.loaders.from_snipmate").lazy_load()
 
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
--- ╔╦╗┬ ┬  ╔═╗┌┐┌┬┌─┐┌─┐┌─┐┌┬┐┌─┐
--- ║║║└┬┘  ╚═╗││││├─┘├─┘├┤  │ └─┐
--- ╩ ╩ ┴   ╚═╝┘└┘┴┴  ┴  └─┘ ┴ └─┘
+local lorem = [[Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.]]
 
-local date = function() return {os.date('%Y-%m-%d')} end
+local date = function() 
+    return {os.date('%Y-%m-%d')} 
+end
 
-local lorem_ipsum = [[Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ]]
+
+-- ╔═╗┌┐┌┬┌─┐┌─┐┌─┐┌┬┐  ╔╦╗┌─┐┌┐ ┬  ┌─┐
+-- ╚═╗││││├─┘├─┘├┤  │    ║ ├─┤├┴┐│  ├┤ 
+-- ╚═╝┘└┘┴┴  ┴  └─┘ ┴    ╩ ┴ ┴└─┘┴─┘└─┘
 
 luasnip.add_snippets(nil, {
-    all = {
-        snippet({ trig = "date" }, {
-            fn_node(date, {}),
-        }),
 
-        snippet({ trig = "lorem" }, {
-            tx_node(lorem_ipsum, {})
+    all = {
+        
+        luasnip.parser.parse_snippet("lorem", lorem),
+
+        snippet("date", { 
+            fn_node(date, {}), 
         }),
-    }
+    },
+
+
+    lua = { 
+
+        snippet("req", fmt("local {} = require('{}')", {
+            in_node(1, "deafult"),
+            in_node(2, "default") 
+        })),
+
+    },
+
+
+    markdown = { 
+
+        snippet("sec", fmt( "# {}", {
+            in_node(1, "Section")
+        })),
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
