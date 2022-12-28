689d3f69a34652a5395b7d5247dc85383047e68d
Nikin Baidar <nikinbaidarr@gmail.com>
2022-12-28 18:50:50 +0545

    init.lua sectioned

---
+++ config/nvim/lua/core/augroup.lua
@@ -0,0 +1,31 @@
+--
+-- ░█▀█░█░█░█▀▀░█▀▄░█▀█░█░█░█▀█░░░░█░░░█░█░█▀█
+-- ░█▀█░█░█░█░█░█▀▄░█░█░█░█░█▀▀░░░░█░░░█░█░█▀█
+-- ░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░░░▀░░▀▀▀░▀▀▀░▀░▀
+
+local autocmd
+local augroup
+
+autocmd = vim.api.nvim_create_autocmd
+augroup = vim.api.nvim_create_augroup('augroup', {clear = true})
+
+autocmd('BufReadPost', {
+    pattern = '*',
+    group = augroup,
+    desc = 'Jump to the last cursor position.',
+    command = 'normal! g`\"'
+})
+
+autocmd('VimLeave', {
+    pattern = '*',
+    group = augroup,
+    desc = 'Echo nothing so gibberish don\'t print on linux terminal.',
+    command = 'echo ""'
+})
+
+autocmd('TermOpen', {
+    pattern = 'term://*',
+    group = augroup,
+    desc = 'Open built-in terminal in insert mode.',
+    command = 'startinsert'
+})
