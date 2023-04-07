91de2476651c72bd84594906a4b00d08f1c3a53d
Nikin Baidar <nikinbaidarr@gmail.com>
2023-04-07 08:59:19 +0545

    use fzf's buffers

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -8,12 +8,12 @@
 map = vim.api.nvim_set_keymap
 
 vim.g.mapleader = " " 
-map('n', '<leader>d', ':ls<CR>:bd', { noremap = true })
-map('n', '<leader>s', ':ls<CR>:b' , { noremap = true })
-map('n', '<leader>h', ':bp<CR>'   , { noremap = true })
-map('n', '<leader>j', '<C-w>w'    , { noremap = true })
-map('n', '<leader>k', '<C-w>w'    , { noremap = true }) 
-map('n', '<leader>l', ':bn<CR>'   , { noremap = true })
-map('!', '<C-d>'    , '<Del>'     , { noremap = true })
+map('n', '<leader>d', ':ls<CR>:bd'   , { noremap = true })
+map('n', '<leader>s', ':Buffers<CR>' , { noremap = true })
+map('n', '<leader>h', ':bp<CR>'      , { noremap = true })
+map('n', '<leader>j', '<C-w>w'       , { noremap = true })
+map('n', '<leader>k', '<C-w>w'       , { noremap = true }) 
+map('n', '<leader>l', ':bn<CR>'      , { noremap = true })
+map('!', '<C-d>'    , '<Del>'        , { noremap = true })
 
 map('n', '<leader> ', ':set spell!<CR>', { noremap = true })
