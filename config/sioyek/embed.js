3a424539c1a43ac375a4761c8d0dcfe8c73a5eef
Nikin Baidar <nikinbaidarr@gmail.com>
2024-01-29 00:38:07 +0545

    final pdf viewer

--- config/sioyek/embed.js
+++ config/sioyek/embed.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-
+sioyek.embed_annotations();
 sioyek._extract_highlights();
-sioyek.embed_annotations();
+sioyek_async.wait(1000);
 sioyek.delete_all_highlights();
-sioyek.embed_annotations();
-
