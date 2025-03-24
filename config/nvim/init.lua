72f9e77c3484602f7ecf664683d7bad67f67e08e
nikinbaidar <nikinbaidarr@gmail.com>
2025-03-24 15:56:03 +0545

    fix moncon

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,13 +3,6 @@
 -- ░░█░░█░█░░█░░░█░░░░░█░░░█░█░█▀█
 -- ░▀▀▀░▀░▀░▀▀▀░░▀░░▀░░▀▀▀░▀▀▀░▀░▀
 
-require("core.plugins")
 require("core.options")
-require("core.keymaps")
-require("core.augroup")
+vim.cmd "colorscheme quiet"
 
-vim.cmd "colorscheme vim"
-vim.cmd "TSContextDisable"
-
-vim.cmd [[ hi TreeSitterContext guibg=#135D66 gui=reverse,BOLD ]]
-vim.cmd [[ hi StatusLine guibg=NONE ]]
--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -3,27 +3,6 @@
 -- ░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█░░░█░░░█░█░█▀█
 -- ░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀▀▀░▀▀▀░▀░▀
 
-vim.opt.hlsearch       = false
-vim.opt.swapfile       = false
 vim.opt.clipboard      = "unnamedplus"
 vim.opt.number         = true
 vim.opt.relativenumber = true
-vim.opt.cursorline     = true
-vim.opt.ignorecase     = true
-vim.opt.smartcase      = true
-vim.opt.autochdir      = true
-vim.opt.expandtab      = true
--- vim.opt.breakindent    = true
-vim.opt.laststatus    = 3
-vim.opt.winbar    = "%m  %f"
-vim.opt.linebreak      = true 
-vim.opt.termguicolors  = true
-vim.opt.scrolloff      = 5
-vim.opt.shiftwidth     = 4
-vim.opt.showmode     = false
-vim.opt.tabstop        = 4
-vim.opt.softtabstop    = 4
-vim.opt.textwidth      = 79
--- vim.opt.colorcolumn    = "80"
-vim.opt.path           = "80"
-vim.opt.path           = ".,**,/usr/include,,"
