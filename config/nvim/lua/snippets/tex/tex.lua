2e8267505364ed08f42871c24e784662ee4ed926
nikinbaidar <nikinbaidarr@gmail.com>
2026-01-10 17:26:36 +0545

    tex smips

--- config/nvim/lua/snippets/tex/tex.lua
+++ config/nvim/lua/snippets/tex/tex.lua
@@ -1,8 +1,24 @@
+local item = "\\item "
+
 return {
-    s({trig="$$", snippetType="autosnippet", desc={"Inline math"}, {
+    parse({trig="latex", desc="LaTeX"}, "\\LaTeX"),
+
+    s({trig="dcl", snippetType="autosnippet", desc="document class"}, {
+        t("\\documentclass[a4 paper,"), i(2), t("]{"), i(1), t("}"),
+    }),
+
+    s({trig="$$", snippetType="autosnippet", desc="Inline math"}, {
         t"$",
         i(1),
         t"$",
-        i(2)
+        i(2),
+    }),
+
+    s({trig="beg", desc="Begin an environment"}, {
+        t"\\begin{", i(1), t{"}", "\t"},
+        m(1, "itemize", item),
+        m(1, "enumerate", item),
+        i(2),
+        t{"", "\\end{"}, rep(1), t"}",
     }),
 }
