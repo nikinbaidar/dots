e0df395f4a6ce4e438916a0b6bfc97981ff37934
Nikin Baidar <nikinbaidarr@gmail.com>
2026-05-04 09:49:06 +0545

    disables tressitter for it has wierd erros and I got not tie time to fix them

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,11 +1,11 @@
-require 'nvim-treesitter'.setup { 
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = { "markdown", "tex", "plaintex" }
-    },
-
-    contex_commentstring = {
-        enable = true
-    },
-} 
+-- require 'nvim-treesitter'.setup { 
+--     highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = false,
+--         disable = { "markdown", "tex", "plaintex" }
+--     },
+--
+--     contex_commentstring = {
+--         enable = true
+--     },
+-- } 
