464c25575848bb83df82fc3458ea8849c07c7a50
Nikin Baidar <nikinbaidarr@gmail.com>
2026-05-08 21:04:36 +0545

    adds quarto specific snippets

---
+++ config/nvim/lua/snippets/quarto.lua
@@ -0,0 +1,10 @@
+return {
+    s("yaml", {
+        t({"---", ""}),
+        t("title: "),
+        i(1),
+        t({"", "author:", "\tname: Nikin Baidar", "\turl: ", ""}),
+        t({"date: today", ""}),
+        t({"---", ""})
+    }),
+}
