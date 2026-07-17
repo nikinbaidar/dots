24b486d0416bff1b0bf7ed2670e39cf6a8b3679c
Nikin Baidar <nikinbaidarr@gmail.com>
2026-07-17 23:48:09 +0545

    lsp config

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,6 +1,37 @@
-vim.lsp.enable({
-    "pyright",
-    "ts_ls",
+-- See `:help vim.lsp.config`.
+--
+--
+
+vim.lsp.config['pyright'] = {
+    cmd = { 'pyright-langserver', '--stdio'},
+    filetypes = { 'python' },
+    root_makrs = {},
+}
+
+
+vim.diagnostic.config({
+    float = { border = "single", },
 })
 
-vim.diagnostic.enable(false)
+
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.diagnostic.open_float(nil, {
+            focus = false,
+            scope = "cursor",
+        })
+    end,
+})
+
+
+vim.opt.updatetime = 250
+
+
+
+-- vim.api.nvim_create_user_command(
+--     "ShowDiagnostics",
+--     function() vim.diagnostic.open_float() end,
+--     {desc = "Display LSP diagnostics"}
+-- )
+
+vim.lsp.enable({"pyright"})
