09c9920b02905a0f0a07e625707815317bdd1caf
Nikin Baidar <nikinbaidarr@gmail.com>
2026-07-31 13:29:04 +0545

    use diagnostics icons

--- config/nvim/lua/config/diagnostics.lua
+++ config/nvim/lua/config/diagnostics.lua
@@ -1,7 +1,14 @@
-local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "", }
+#!/usr/bin/env lua
 
+local signs = {
+    [vim.diagnostic.severity.ERROR] = "✗",
+    [vim.diagnostic.severity.WARN]  = "",
+    [vim.diagnostic.severity.INFO]  = "",
+    [vim.diagnostic.severity.HINT]  = "󰌵",
+}
 vim.diagnostic.config({
     float = { border = "single", },
+    signs = { text = signs, },
 })
 
 vim.api.nvim_create_autocmd("CursorHold", {
