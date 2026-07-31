f0af138368e5f375cf36adf730adfad379793a3a
Nikin Baidar <nikinbaidarr@gmail.com>
2026-07-31 09:23:50 +0545

    don't expand on tab

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -7,6 +7,7 @@ cmp.setup({
     },
     keymap = {
         [ '<CR>' ] = { 'select_and_accept', 'fallback' },
+        [ '<Tab>' ] = { 'fallback' },
     },
     sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
